8cff331e6daed6537ab4e4b88f79dcf0ea3ffb5b
Torsten Simon <simon@edu-sharing.net>
2025-11-12 09:52:36 +0100

    allow frame embeddings chrome extension

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ app.use(helmet({
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'", "http://localhost:3000", "http://localhost:3001"],
       frameSrc: ["'self'"],
-      frameAncestors: ["* chrome-extension://randomletters"],
+      frameAncestors: ["* chrome-extension://*"],
     },
   },
   crossOriginEmbedderPolicy: false,
